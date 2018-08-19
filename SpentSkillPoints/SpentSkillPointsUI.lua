e4027dff35ce5834a901660cf8cba1b8c26802e4
Shinni <shinni@binaryvector.net>
2018-08-19 20:45:52 +0200

    fixes for wolf hunter update

--- SpentSkillPoints/SpentSkillPointsUI.lua
+++ SpentSkillPoints/SpentSkillPointsUI.lua
@@ -11,8 +11,8 @@ end
 
 -- replace the controls which display the skill lines on the left part of the skill window
 -- with my own control
-local oldAddNode = SKILLS_WINDOW.navigationTree.AddNode
-SKILLS_WINDOW.navigationTree.AddNode = function( self, template, data, parentNode, selectSound )
+local oldAddNode = SKILLS_WINDOW.skillLinesTree.AddNode
+SKILLS_WINDOW.skillLinesTree.AddNode = function( self, template, data, parentNode, selectSound )
 	if template == "ZO_SkillIconHeader" then
 		return oldAddNode( self, "SSP_Header", data, parentNode, selectSound )
 	elseif template == "ZO_SkillsNavigationEntry" then
@@ -22,37 +22,42 @@ SKILLS_WINDOW.navigationTree.AddNode = function( self, template, data, parentNod
 end
 
 -- when a skill type (World, Class etc) control is initialized/refreshed, then add the spent skill points to it
-local header = SKILLS_WINDOW.navigationTree.templateInfo["ZO_SkillIconHeader"]
+local header = SKILLS_WINDOW.skillLinesTree.templateInfo["ZO_SkillIconHeader"]
 local headerFunction = header.setupFunction
-local function TreeHeaderSetup(node, control, skillType, open)
-	headerFunction(node, control, skillType, open)
+local function TreeHeaderSetup(node, control, skillTypeData, open)
+	headerFunction(node, control, skillTypeData, open)
 	local label = control:GetNamedChild("PointText")
+	local skillType = skillTypeData:GetSkillType()
 	label:SetText(SSP.GetTypeSpentPoints( skillType )) 
 end
-SKILLS_WINDOW.navigationTree:AddTemplate("SSP_Header", TreeHeaderSetup, nil, nil, nil, 0)
+SKILLS_WINDOW.skillLinesTree:AddTemplate("SSP_Header", TreeHeaderSetup, nil, nil, nil, 0)
 
-local navigationSetup = SKILLS_WINDOW.navigationTree.templateInfo["ZO_SkillsNavigationEntry"].setupFunction
-local navigationSelect = SKILLS_WINDOW.navigationTree.templateInfo["ZO_SkillsNavigationEntry"].selectionFunction
-local navigationEQ = SKILLS_WINDOW.navigationTree.templateInfo["ZO_SkillsNavigationEntry"].equalityFunction
+local navigationSetup = SKILLS_WINDOW.skillLinesTree.templateInfo["ZO_SkillsNavigationEntry"].setupFunction
+local navigationSelect = SKILLS_WINDOW.skillLinesTree.templateInfo["ZO_SkillsNavigationEntry"].selectionFunction
+local navigationEQ = SKILLS_WINDOW.skillLinesTree.templateInfo["ZO_SkillsNavigationEntry"].equalityFunction
 -- the function which initalizes/refreshes the skill line control
 local function TreeEntrySetup(node, control, data, open)
+	local skillType, skillLineIndex = data:GetIndices()
 	-- initialize the control with skill line name etc
 	navigationSetup(node, control, data, open)
 	-- now we add our custom information
 	local label = control:GetNamedChild("PointText")
 	-- do we want to colorize the skill line?
 	if SSP.settings.color then
+		-- todo, i should probably use this
+		--SKILL_POINT_ALLOCATION_MANAGER:GetNumPointsAllocatedInSkillLine(self)
 		-- add the colorized number of spent skill points in this skill line
-		local quote = SSP.GetLineSpentPoints( data.skillType, data.skillLineIndex ) / SSP.GetLinePossiblePoints( data.skillType, data.skillLineIndex )
+		local quote = SSP.GetLineSpentPoints( skillType, skillLineIndex ) / SSP.GetLinePossiblePoints( skillType, skillLineIndex )
 		local red = 255 * (1-quote)
 		local green = 255 * quote
 		label:SetText(
-			string.format("|c%02x%02x%02x%d|r", red, green, 0, SSP.GetLineSpentPoints( data.skillType, data.skillLineIndex ))
+			string.format("|c%02x%02x%02x%d|r", red, green, 0, SSP.GetLineSpentPoints( skillType, skillLineIndex ))
 			)
 		-- add the colorized rank for this skill line
 		label = control:GetNamedChild("LevelText")
-		local _, rank = GetSkillLineInfo(data.skillType, data.skillLineIndex)
-		quote = rank / SSP.GetMaxRank(data.skillType, data.skillLineIndex)
+		local rank = data:GetCurrentRank()
+		--local _, rank = GetSkillLineInfo(skillType, skillLineIndex)
+		quote = rank / SSP.GetMaxRank(skillType, skillLineIndex)
 		red = 255 * (1-quote)
 		green = 255 * quote
 		label:SetText(
@@ -60,11 +65,11 @@ local function TreeEntrySetup(node, control, data, open)
 			)
 	else
 		-- add number of spent skill points and the rank of the skill line
-		label:SetText(SSP.GetLineSpentPoints( data.skillType, data.skillLineIndex ))
+		label:SetText(SSP.GetLineSpentPoints( skillType, skillLineIndex ))
 		label = control:GetNamedChild("LevelText")
-		local _, rank = GetSkillLineInfo(data.skillType, data.skillLineIndex)
+		local _, rank = GetSkillLineInfo(skillType, skillLineIndex)
 		label:SetText(rank)
 	end
 end
 
-SKILLS_WINDOW.navigationTree:AddTemplate("SSP_NavigationEntry", TreeEntrySetup, navigationSelect, navigationEQ)
+SKILLS_WINDOW.skillLinesTree:AddTemplate("SSP_NavigationEntry", TreeEntrySetup, navigationSelect, navigationEQ)
